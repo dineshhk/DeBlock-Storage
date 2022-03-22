9b3d553c140b100efca5f69cb5c0407dc6c286a7
Dinesh K Lohar <55583788+dineshhk@users.noreply.github.com>
2022-03-23 02:09:43 +0530

    Add files via upload

---
+++ build/precache-manifest.605944426e516f65354f247ca8bad356.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2232915e44e1156e4df84a887075df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a06050a54910b2880fb",
+    "url": "./static/css/2.9b12e5dc.chunk.css"
+  },
+  {
+    "revision": "b1ff1dfb275dd827956c",
+    "url": "./static/css/main.d2ecfd4f.chunk.css"
+  },
+  {
+    "revision": "3a06050a54910b2880fb",
+    "url": "./static/js/2.6e5f5e47.chunk.js"
+  },
+  {
+    "revision": "01165c694011839f3f18c399af979d77",
+    "url": "./static/js/2.6e5f5e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ff1dfb275dd827956c",
+    "url": "./static/js/main.5bb57336.chunk.js"
+  },
+  {
+    "revision": "3ffb6404ac2f2bd77c5b",
+    "url": "./static/js/runtime-main.9cc47200.js"
+  },
+  {
+    "revision": "5753c50287422b131ecb7f658110a357",
+    "url": "./static/media/logo.5753c502.png"
+  }
+]);
